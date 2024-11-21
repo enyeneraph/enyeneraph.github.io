bca7aa37f6d74893dfb58ce5078fdccdd818a9bc
Ebenezer Raphael <ebenezerraphael0@gmail.com>
2024-11-21 06:55:06 +0100

    Add files via upload

---
+++ posts/new-post.md
@@ -0,0 +1,1 @@
+<!-- This is where the content of your post/notes goes. -->
--- posts/posts.js
+++ posts/posts.js
@@ -1,1 +1,14 @@
+// To add a new post, you MUST add its meta data. If you don't do this, the post will not display on the page.
 
+// Follow the template below to include the meta data of a post.
+
+// ADD NEW POSTS AT THE TOP!!!
+
+const posts = [
+     /** {
+          title: 'Title', // Post title
+          date: '2024-01-31', // Post data using this format
+          file: 'file-name.md', // The file name
+          excerpt: '...' // This is not important. You can leave it as is.
+     } **/
+];
